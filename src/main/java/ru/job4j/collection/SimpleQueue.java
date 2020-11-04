b87a591541f9af2585f96a63a1ef0ea5caf80610
AlexeyEsipov <a_esipov@list.ru>
2020-11-04 19:16:52 +0300

    5. Очередь на двух стеках [#379881]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -12,7 +12,6 @@ public class SimpleQueue<T> {
         if (countIn == 0 && countOut == 0) {
             throw new NoSuchElementException();
         }
-        T result = null;
         if (countOut == 0) {
            while (countIn > 0) {
                out.push(in.pop());
@@ -20,11 +19,8 @@ public class SimpleQueue<T> {
                countIn--;
            }
         }
-        if (countOut > 0) {
-            result = out.pop();
-            countOut--;
-        }
-        return result;
+        countOut--;
+        return out.pop();
     }
 
     public void push(T value) {
