d465e625372a0391b455367502229427713758aa
AlexeyEsipov <a_esipov@list.ru>
2020-11-09 22:08:42 +0300

    4. Переопределить только equals [#379910]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -26,23 +26,22 @@ public class User {
                 + ", birthday=" + birthday.getTime() + '}';
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) {
-//            return true;
-//        }
-//        if (o == null || getClass() != o.getClass()) {
-//            return false;
-//        }
-//        User user = (User) o;
-//        return children == user.children
-//               && name.equals(user.name)
-//               && birthday.equals(user.birthday);
-//    }
-
-    @SuppressWarnings("checkstyle:EqualsHashCode")
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+               && name.equals(user.name)
+               && birthday.equals(user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 }
