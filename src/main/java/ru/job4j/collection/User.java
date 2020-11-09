005acf049dc91f6e8f749283d58b9aa1e7cfa8c0
AlexeyEsipov <a_esipov@list.ru>
2020-11-09 18:40:06 +0300

    1. Реализовать коллекцию Set на массиве [#379904]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,47 @@
+package ru.job4j.collection;
+
+import java.util.Calendar;
+import java.util.Objects;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public User(String name, Calendar birthday) {
+        this (name, 0, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children
+               && name.equals(user.name)
+               && birthday.equals(user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+}
