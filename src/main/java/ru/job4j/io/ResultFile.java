a60a447e3b1953ad59dd28c705d025b4897dcf46
AlexeyEsipov <a_esipov@list.ru>
2020-12-15 13:51:58 +0300

    0.2. FileInputStream [#379937]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+    try (FileOutputStream out = new FileOutputStream("result.txt")) {
+        out.write("Hello, world!".getBytes());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    try (FileOutputStream out = new FileOutputStream("input.txt")) {
+        out.write("login=login".getBytes());
+        out.write("password=password".getBytes());
+        out.write("login=login".getBytes());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+    try (FileOutputStream out = new FileOutputStream("even.txt")) {
+        out.write("1".getBytes());
+        out.write("5".getBytes());
+        out.write("15".getBytes());
+        out.write("17".getBytes());
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+
+}
+}
