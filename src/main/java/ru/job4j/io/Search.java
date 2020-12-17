1c7504bb683669be48ef993897d9042426468f6e
AlexeyEsipov <a_esipov@list.ru>
2020-12-17 22:35:26 +0300

    5. Валидация параметров запуска. [#379926]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,9 +10,14 @@ import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Не указаны параметры. Укажите папку для поиска и "
+                    + "через пробел требуемое расширение файла, например, C:\\ .txt");
+        }
+        Path start = Paths.get(args[0]);
+        String ex = args[1];
         System.out.println(start.toString());
-        search(start, "txt").forEach(System.out::println);
+        search(start, ex).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
