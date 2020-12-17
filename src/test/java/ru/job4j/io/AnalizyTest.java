ee4d4f16870993b4b176caee4a0c8259785ebd50
AlexeyEsipov <a_esipov@list.ru>
2020-12-17 12:53:20 +0300

    3.0. Тестирование IO [#379925]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -11,10 +13,15 @@ import static org.junit.Assert.*;
 
 public class AnalizyTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void unavailable()  {
+    public void unavailable() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(
-                     new FileOutputStream("source1.log")))) {
+                     new FileOutputStream(source)))) {
             out.write("200 10:56:01" + System.lineSeparator());
             out.write("500 10:57:01" + System.lineSeparator());
             out.write("400 10:58:01" + System.lineSeparator());
@@ -25,8 +32,8 @@ public class AnalizyTest {
             e.printStackTrace();
         }
         Analizy analizy = new Analizy();
-        analizy.unavailable("source1.log", "target.log");
-        try (BufferedReader in = new BufferedReader(new FileReader("target.log"))) {
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             List<String> lin = new ArrayList<>();
             in.lines().forEach(lin::add);
             assertThat(lin.get(0), is("10:57:01;10:59:01"));
