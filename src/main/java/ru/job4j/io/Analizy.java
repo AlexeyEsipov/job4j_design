92447d30f3a668768c447c7ac6d927f91b8cccf4
AlexeyEsipov <a_esipov@list.ru>
2020-12-19 15:33:11 +0300

    2. Анализ доступности сервера. [#379934]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -7,26 +7,15 @@ public class Analizy {
     public void unavailable(String source, String target) {
         List<String> lines = new ArrayList<>();
         StringBuilder sb = new StringBuilder();
-        boolean workTime = false;
-        int i = 0;
+        boolean workTime = true;
         try (BufferedReader in = new BufferedReader(new FileReader(source))) {
             for (String line = in.readLine(); line != null; line = in.readLine()) {
-                if ((i++) == 0) {
-                    if (line.startsWith("400") || line.startsWith("500")) {
-                        sb.append(line.split(" ")[1]);
-                        sb.append(";");
-                    } else {
-                        workTime = true;
-                    }
-                    continue;
+                if (workTime && (line.startsWith("400") || line.startsWith("500"))) {
+                    workTime = false;
+                    sb.append(line.split(" ")[1]);
+                    sb.append(";");
                 }
-                if (workTime) {
-                    if (line.startsWith("400") || line.startsWith("500")) {
-                        workTime = false;
-                        sb.append(line.split(" ")[1]);
-                        sb.append(";");
-                    }
-                } else if (line.startsWith("200") || line.startsWith("300")) {
+                if (!workTime && (line.startsWith("200") || line.startsWith("300"))) {
                     workTime = true;
                     sb.append(line.split(" ")[1]);
                     lines.add(sb.toString());
@@ -36,6 +25,7 @@ public class Analizy {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         try (PrintWriter out = new PrintWriter(new BufferedOutputStream(
                      new FileOutputStream(target)))) {
             for (String line : lines) {
@@ -49,11 +39,5 @@ public class Analizy {
     public static void main(String[] args) {
         Analizy analizy = new Analizy();
         analizy.unavailable("server.log", "target.log");
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:11:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
