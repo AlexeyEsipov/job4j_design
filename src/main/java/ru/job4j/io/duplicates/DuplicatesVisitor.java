b4439c00a23c1e1a648a8d68349d8f290fd8fb78
AlexeyEsipov <a_esipov@list.ru>
2021-02-17 15:35:04 +0300

    4.2. Поиск дубликатов [#379932]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,6 +1,5 @@
 package ru.job4j.io.duplicates;
 
-
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
@@ -9,8 +8,8 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private final Map<FileProperty, Path> validateMap = new HashMap<>();
-    private final Map<FileProperty, HashSet<Path>> duplicateMap = new HashMap<>();
+    private final Set<FileProperty> validateSet = new HashSet<>();
+    private final Map<FileProperty, Path> duplicateMap = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
@@ -23,28 +22,20 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
         long size = attrs.size();
         FileProperty nameSize = new FileProperty(name, size);
         Path path = file.toAbsolutePath();
-        if (validateMap.putIfAbsent(nameSize, path) != null) {
-            HashSet<Path> set = new HashSet<>();
-            if (duplicateMap.containsKey(nameSize)) {
-                set = duplicateMap.get(nameSize);
-            } else {
-                set.add(validateMap.get(nameSize));
-            }
-            set.add(path);
-            duplicateMap.put(nameSize, set);
+        if (!validateSet.add(nameSize)) {
+            duplicateMap.put(nameSize, path);
         }
     }
 
     public void printDuplicateMap() {
-        Set<Map.Entry<FileProperty, HashSet<Path>>> set = duplicateMap.entrySet();
+        Set<Map.Entry<FileProperty, Path>> set = duplicateMap.entrySet();
         System.out.println("Duplicates:");
-        for (Map.Entry<FileProperty, HashSet<Path>> item : set) {
+        for (Map.Entry<FileProperty, Path> item : set) {
             System.out.println("name: " + item.getKey().getName()
                             + "; size: " + item.getKey().getSize());
-            for (Path path : item.getValue()) {
-                System.out.println("path " + path.toString());
-            }
-            System.out.println(" ");
+            System.out.println("path " + item.getValue().toString());
         }
+        System.out.println(" ");
     }
 }
+
