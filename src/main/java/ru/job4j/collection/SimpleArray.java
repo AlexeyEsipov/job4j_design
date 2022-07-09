261f6eb581190880b7e7114ecef03d9cb10e2001
AlexeyEsipov <a_esipov@list.ru>
2022-07-09 22:56:01 +0300

    Обновил класс SimpleArray

--- src/main/java/ru/job4j/collection/SimpleArray.java
+++ src/main/java/ru/job4j/collection/SimpleArray.java
@@ -2,68 +2,97 @@ package ru.job4j.collection;
 
 import java.util.*;
 
-public class SimpleArray<T> implements Iterable<T> {
-    private Object[] container;
-    private int indexFree;
-    private int modCount = 0;
+public class SimpleArray<T> implements SimpleList<T> {
+    private T[] container;
+    private int size = 0;
+    private int modCount;
 
-    public SimpleArray(int length) {
-        this.container = new Object[length];
-        this.indexFree = 0;
+    public SimpleArray(int capacity) {
+        this.container = (T[]) new Object[capacity];
     }
 
     public SimpleArray() {
-        this(10);
+        this(0);
     }
 
+    @Override
     public void add(T model) {
-        if (indexFree == container.length) {
+        if (size == container.length) {
             grow();
         }
-        container[indexFree++] = model;
+        container[size++] = model;
         modCount++;
     }
 
+    @Override
+    public T set(int index, T newValue) {
+        T oldValue = get(index);
+        container[index] = newValue;
+        return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        T value = get(index);
+        System.arraycopy(container, index + 1, container, index, size - index - 1);
+        container[--size] = null;
+        modCount++;
+        return value;
+    }
+
+    @Override
     public T get(int index) {
-        Objects.checkIndex(index, indexFree);
-        return (T) container[index];
+        Objects.checkIndex(index, size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
     }
 
     @Override
     public Iterator<T> iterator() {
-        return new ContIterator<>();
+        return new Iterator<>() {
+            private int index = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return  container[index++];
+            }
+        };
     }
 
     private void grow() {
         if (container.length == 0) {
             container = Arrays.copyOf(container, 1);
-            return;
+        } else {
+            container = Arrays.copyOf(container, container.length * 2);
         }
-        container = Arrays.copyOf(container, container.length * 2);
         modCount++;
     }
 
-    private class ContIterator<T> implements Iterator<T> {
-        private int nextElement = 0;
-        private final int expectedModCount = modCount;
+    public static void main(String[] args) {
+        SimpleArray<Integer> sa = new SimpleArray<>(6);
+        sa.add(1);
+        sa.add(2);
+        sa.add(3);
+        sa.add(4);
+        sa.add(5);
+        sa.add(6);
+        sa.remove(2);
 
-        @Override
-        public boolean hasNext() {
-            if (expectedModCount != modCount) {
-                throw new ConcurrentModificationException();
-            }
-            return nextElement < indexFree;
-        }
-
-        @Override
-        public T next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
-            }
-            if (expectedModCount != modCount) {
-                throw new ConcurrentModificationException();
-            }
-            return  (T) container[nextElement++];
-        }
     }
 }
--- src/test/java/ru/job4j/collection/SimpleArrayTest.java
+++ src/test/java/ru/job4j/collection/SimpleArrayTest.java
@@ -1,69 +1,139 @@
 package ru.job4j.collection;
 
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.stream.IntStream;
 
 public class SimpleArrayTest {
 
-    @Test
-    public void whenAddThenGet() {
-        SimpleArray<String> array = new SimpleArray<>();
-        array.add("first");
-        String rsl = array.get(0);
-        assertThat(rsl, is("first"));
+    private SimpleList<Integer> list;
+
+    @Before
+    public void initData() {
+        list = new SimpleArray<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
     }
 
     @Test
-    public void whenEmptyAddThenGrowAndGet() {
-        SimpleArray<String> array = new SimpleArray<>(0);
-        array.add("first");
-        String rsl = array.get(0);
-        assertThat(rsl, is("first"));
-        array.add("second");
-        String rsl1 = array.get(1);
-        assertThat(rsl1, is("second"));
+    public void whenAddThenSizeIncrease() {
+        assertEquals(3, list.size());
     }
 
     @Test
-    public void whenAddThenIt() {
-        SimpleArray<String> array = new SimpleArray<>();
-        array.add("first");
-        String rsl = array.iterator().next();
-        assertThat(rsl, is("first"));
+    public void whenAddAndGetByCorrectIndex() {
+        assertEquals(Integer.valueOf(1), list.get(0));
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void whenGetEmpty() {
-        SimpleArray<String> array = new SimpleArray<>();
-        array.get(0);
+    public void whenGetByIncorrectIndexThenGetException() {
+        list.get(5);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
-    public void whenGetOutBound() {
-        SimpleArray<String> array = new SimpleArray<>();
-        array.add("first");
-        array.get(1);
+    public void whenAndAndGetByIncorrectIndexThenGetException() {
+        SimpleList<Integer> list = new SimpleArray<>(10);
+        list.add(5);
+        list.get(5);
+    }
+
+    @Test
+    public void whenRemoveThenGetValueAndSizeDecrease() {
+        assertEquals(3, list.size());
+        assertEquals(Integer.valueOf(2), list.remove(1));
+        assertEquals(2, list.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenRemoveByIncorrectIndexThenGetException() {
+        list.remove(5);
+    }
+
+    @Test
+    public void whenRemoveThenMustNotBeEmpty() {
+        list.remove(1);
+        assertEquals(Integer.valueOf(1), list.get(0));
+        assertEquals(Integer.valueOf(3), list.get(1));
+    }
+
+    @Test
+    public void whenAddNullThenMustBeSameBehavior() {
+        list = new SimpleArray<>(3);
+        list.add(null);
+        list.add(null);
+        assertEquals(2, list.size());
+        assertNull(list.get(0));
+        assertNull(list.get(1));
+    }
+
+    @Test
+    public void whenSetThenGetOldValueAndSizeNotChanged() {
+        assertEquals(Integer.valueOf(2), list.set(1, 22));
+        assertEquals(3, list.size());
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void whenSetByIncorrectIndexThenGetException() {
+        list.set(5, 22);
+    }
+
+    @Test
+    public void whenGetIteratorFromEmptyListThenHasNextReturnFalse() {
+        list = new SimpleArray<>(5);
+        assertFalse(list.iterator().hasNext());
     }
 
     @Test(expected = NoSuchElementException.class)
-    public void whenGetEmptyFromIt() {
-        SimpleArray<String> array = new SimpleArray<>();
-        array.iterator().next();
+    public void whenGetIteratorFromEmptyListThenNextThrowException() {
+        list = new SimpleArray<>(5);
+        list.iterator().next();
+    }
+
+    @Test
+    public void whenGetIteratorTwiceThenStartAlwaysFromBeginning() {
+        assertEquals(Integer.valueOf(1), list.iterator().next());
+        assertEquals(Integer.valueOf(1), list.iterator().next());
+    }
+
+    @Test
+    public void whenCheckIterator() {
+        Iterator<Integer> iterator = list.iterator();
+        assertTrue(iterator.hasNext());
+        assertEquals(Integer.valueOf(1), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(Integer.valueOf(2), iterator.next());
+        assertTrue(iterator.hasNext());
+        assertEquals(Integer.valueOf(3), iterator.next());
+        assertFalse(iterator.hasNext());
+    }
+
+    @Test
+    public void whenNoPlaceThenMustIncreaseCapacity() {
+        assertSame(3, list.size());
+        IntStream.range(3, 10).forEach(v -> list.add(v));
+        assertSame(10, list.size());
     }
 
     @Test(expected = ConcurrentModificationException.class)
-    public void whenCorruptedIt() {
-        SimpleArray<String> array = new SimpleArray<>();
-        array.add("first");
-        Iterator<String> it = array.iterator();
-        array.add("second");
-        it.next();
+    public void whenAddAfterGetIteratorThenMustBeException() {
+        Iterator<Integer> iterator = list.iterator();
+        list.add(4);
+        iterator.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void whenRemoveAfterGetIteratorThenMustBeException() {
+        list.add(0);
+        Iterator<Integer> iterator = list.iterator();
+        list.remove(0);
+        iterator.next();
     }
 
 }
