cb9b7ddd7bd708360f7eabda881b72610f98e008
AlexeyEsipov <a_esipov@list.ru>
2022-08-20 01:54:16 +0300

    1 fail

--- src/test/java/ru/job4j/assertj/NameLoadTest.java
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -1,7 +1,7 @@
 package ru.job4j.assertj;
 
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class NameLoadTest {
     @Test
@@ -10,5 +10,13 @@ class NameLoadTest {
         assertThatThrownBy(nameLoad::getMap)
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkMap() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "Key=value";
+        nameLoad.parse(name);
+        assertThat(nameLoad.getMap()).containsEntry("Key","value");
     }
 }
