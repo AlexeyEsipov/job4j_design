fc08d6bb7c3e787f3cdcf3d1a60283011b27c444
AlexeyEsipov <a_esipov@list.ru>
2022-08-20 10:10:17 +0300

    2 files

--- src/test/java/ru/job4j/assertj/SimpleConvertTest.java
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -3,6 +3,8 @@ package ru.job4j.assertj;
 import org.assertj.core.data.Index;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleConvertTest {
@@ -15,5 +17,14 @@ class SimpleConvertTest {
                 .contains("first", Index.atIndex(0))
                 .containsAnyOf("zero", "second", "six")
                 .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("1","2","3");
+        assertThat(map.get("1")).isEqualTo(0);
+        assertThat(map.get("2")).isEqualTo(1);
+        assertThat(map.get("3")).isEqualTo(2);
     }
 }
